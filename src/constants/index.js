91227b28cc1914505c6ad92afa9b0cbb621a07bc
yashPundhir <yash.pundhir.prof@gmail.com>
2023-03-13 21:34:50 +0530

    Experience Section Completed

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,11 +42,11 @@ export const navLinks = [
 
 const services = [
 	{
-		title: "Web Developer",
+		title: "UI Developer",
 		icon: web,
 	},
 	{
-		title: "React Native Developer",
+		title: "Frontend Developer",
 		icon: mobile,
 	},
 	{
