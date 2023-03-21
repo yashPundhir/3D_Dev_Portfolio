7fc3387ae6c079bcf0e94e12fc539fd1db9209bf
yashPundhir <yash.pundhir.prof@gmail.com>
2023-03-21 23:10:45 +0530

    Tech Stack Section Updated

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,14 +4,20 @@ import { technologies } from "../constants";
 
 const Tech = () => {
 	return (
-		<div className="flex flex-row flex-wrap justify-center gap-10">
-			{technologies.map((technology) => (
-				<div className=" w-28  h-28" key={technology.name}>
-					<BallCanvas icon={technology.icon} />
-				</div>
-			))}
+		//styling left: do it afterwards
+		<div>
+			{/* <div>
+				<h2>Tech Stack Used:</h2>
+			</div> */}
+			<div className="flex flex-row flex-wrap justify-center gap-10">
+				{technologies.map((technology) => (
+					<div className=" w-28  h-28" key={technology.name}>
+						<BallCanvas icon={technology.icon} />
+					</div>
+				))}
+			</div>
 		</div>
 	);
 };
 
-export default SectionWrapper(Tech, "");
+export default SectionWrapper(Tech, "tech");
