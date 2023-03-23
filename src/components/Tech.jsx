2299c27e504c2489e7ca8551276c54770a605106
yashPundhir <yash.pundhir.prof@gmail.com>
2023-03-23 13:11:34 +0530

    Tech Stack Section updated

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,14 +1,17 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { styles } from "../styles";
 
 const Tech = () => {
 	return (
 		//styling left: do it afterwards
 		<div>
-			{/* <div>
-				<h2>Tech Stack Used:</h2>
-			</div> */}
+			<div
+				className={`${styles.sectionHeadText} mb-16 flex justify-center items-center`}
+			>
+				<h2>Tech &nbsp;Stack</h2>
+			</div>
 			<div className="flex flex-row flex-wrap justify-center gap-10">
 				{technologies.map((technology) => (
 					<div className=" w-28  h-28" key={technology.name}>
