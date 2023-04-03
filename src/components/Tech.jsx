02451e9ce5b23e3556b84a1ace5feb0be781876b
yashPundhir <yash.pundhir.prof@gmail.com>
2023-04-03 15:41:17 +0530

    TextVariant Motion added to Tech Stack heading

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,4 +1,6 @@
 import { BallCanvas } from "./canvas";
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motion";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 import { styles } from "../styles";
@@ -7,11 +9,12 @@ const Tech = () => {
 	return (
 		//styling left: do it afterwards
 		<div>
-			<div
+			<motion.div
+				variants={textVariant()}
 				className={`${styles.sectionHeadText} mb-16 flex justify-center items-center`}
 			>
 				<h2>Tech &nbsp;Stack</h2>
-			</div>
+			</motion.div>
 			<div className="flex flex-row flex-wrap justify-center gap-10">
 				{technologies.map((technology) => (
 					<div className=" w-28  h-28" key={technology.name}>
