fb9ef8867cd98f1b373122356afcc9bd9e0cd3f8
yashPundhir <yash.pundhir.prof@gmail.com>
2023-04-06 23:58:40 +0530

    Outlet component added for Project cards in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,28 @@ import {
 	Works,
 	StarsCanvas,
 } from "./components";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<div className="relative z-0 bg-primary">
-				<div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-					<Navbar />
-					<Hero />
-				</div>
-				<About />
-				<Experience />
-				<Tech />
-				<Works />
-				<Feedbacks />
-				<div className="relative z-0">
-					<Contact />
-					<StarsCanvas />
-				</div>
+		//<BrowserRouter>
+		<div className="relative z-0 bg-primary">
+			<div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+				<Navbar />
+				<Hero />
 			</div>
-		</BrowserRouter>
+			<About />
+			<Experience />
+			<Tech />
+			<Works />
+			<Outlet />
+			<Feedbacks />
+			<div className="relative z-0">
+				<Contact />
+				<StarsCanvas />
+			</div>
+		</div>
+		//</BrowserRouter>
 	);
 };
 
