a2fcf8621ca99dea6046f4b941c5f5eeda884aad
yashPundhir <yash.pundhir.prof@gmail.com>
2023-04-06 23:59:33 +0530

    projects cards part removed from projects section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -8,75 +8,9 @@ import { SectionWrapper } from "../hoc";
 import { htmlCssProjects, tailwindProjects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 import { Link } from "react-router-dom";
-
-const ProjectCard = ({
-	index,
-	name,
-	description,
-	tags,
-	image,
-	source_code_link,
-	live_link,
-}) => {
-	return (
-		<Tilt>
-			<motion.div
-				variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-				className=" shadow-card p-[1px] green-pink-gradient rounded-2xl"
-			>
-				<div
-					options={{
-						max: 45,
-						scale: 1,
-						speed: 450,
-					}}
-					className=" bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-				>
-					<div className=" relative w-full h-[230px]">
-						<img
-							src={image}
-							alt={name}
-							className="w-full h-full object-cover rounded-2xl"
-						/>
-						<div className=" absolute inset-0 flex gap-2 justify-end m-3 card-img_hover">
-							<div
-								className="cursor-pointer black-gradient  w-10  h-10  rounded-full flex justify-center items-center"
-								onClick={() => window.open(source_code_link, "_blank")}
-							>
-								<img
-									src={github}
-									alt="github"
-									className=" w-7 h-7 object-contain"
-								/>
-							</div>
-							<div
-								className="cursor-pointer black-gradient  w-10  h-10  rounded-full flex justify-center items-center"
-								onClick={() => window.open(live_link, "_blank")}
-							>
-								<img
-									src={livelink}
-									alt="github"
-									className=" w-7 h-7 object-contain"
-								/>
-							</div>
-						</div>
-					</div>
-					<div className="mt-5 ml-1">
-						<h3 className="tex-white font-bold text-[24px]">{name}</h3>
-						<p className="mt-2 text-secondary text-[14px]">{description}</p>
-					</div>
-					<div className="mt-4 flex flex-wrap gap-2 ml-1">
-						{tags.map((tag) => (
-							<p key={tag.name} className={`${tag.color} text-[14px]`}>
-								{`#${tag.name}`}
-							</p>
-						))}
-					</div>
-				</div>
-			</motion.div>
-		</Tilt>
-	);
-};
+// import ProjectCardHTMLCSS from "./ProjectCardHTMLCSS";
+import ProjectCardTailwindCSS from "./ProjectCardTailwindCSS";
+import { Outlet } from "react-router-dom";
 
 const Works = () => {
 	// const [active, setActive] = useState(false);
@@ -109,9 +43,10 @@ const Works = () => {
 					and manage projects effectively.
 				</motion.p>
 			</div>
+			{/* Project's categories toggle bar */}
 			<div>
 				<ul
-					className={`list-none flex justify-center items-center gap-10 mt-16 text-2xl font-medium `}
+					className={`list-none flex justify-center items-center gap-10 mt-16 text-2xl font-medium`}
 				>
 					<Link to="/htmlcss">
 						<li
@@ -152,11 +87,6 @@ const Works = () => {
 						</li>
 					</Link>
 				</ul>
-			</div>
-			<div className="mt-20  flex flex-wrap gap-7  justify-center items-center">
-				{activeProjects.map((project, index) => (
-					<ProjectCard key={`project-${index}`} index={index} {...project} />
-				))}
 			</div>
 		</>
 	);
