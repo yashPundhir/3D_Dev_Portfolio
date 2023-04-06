35ff582947cdc6c9fa7ab085f090970448184906
yashPundhir <yash.pundhir.prof@gmail.com>
2023-04-06 23:21:15 +0530

    Routing Configuration added to main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ProjectCardTailwindCSS from "./components/ProjectCardTailwindCSS";
+import ProjectCardHTMLCSS from "./components/ProjectCardHTMLCSS";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const appRouter = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <ProjectCardHTMLCSS />,
+			},
+			{
+				path: "/htmlcss",
+				element: <ProjectCardHTMLCSS />,
+			},
+			{
+				path: "/tailwindcss",
+				element: <ProjectCardTailwindCSS />,
+			},
+			// {
+			// 	path: "/contact",
+			// 	element: (
+			// 		<HeroHome
+			// 			title="Contact Us"
+			// 			link="https://pixlr.com/e/"
+			// 			btnText="Awesome & Free Photo Editing Resource"
+			// 		/>
+			// 	),
+			// },
+		],
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={appRouter} />
+	</React.StrictMode>
+);
