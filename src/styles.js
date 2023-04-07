058f75fdd09af0ea8f9f36486a6c0bab601b98e5
yashPundhir <yash.pundhir.prof@gmail.com>
2023-04-07 21:49:28 +0530

    custom padding for ProjectCards is added to custom css styling file

--- src/styles.js
+++ src/styles.js
@@ -2,6 +2,7 @@ const styles = {
 	paddingX: "sm:px-16 px-6",
 	paddingY: "sm:py-16 py-6",
 	padding: "sm:px-16 px-6 sm:py-16 py-10",
+	paddingPC: "sm:px-16 px-6 sm:pb-16 pb-10 sm:pt-4 pt-2",
 
 	heroHeadText:
 		"font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
