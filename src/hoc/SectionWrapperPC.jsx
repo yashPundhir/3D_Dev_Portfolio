56f746ef94a522cd7b051bcef48126b5afec7796
yashPundhir <yash.pundhir.prof@gmail.com>
2023-04-08 11:29:21 +0530

    Re-render functionality also added to SectionWrapper of ProjectCards

--- src/hoc/SectionWrapperPC.jsx
+++ src/hoc/SectionWrapperPC.jsx
@@ -1,18 +1,34 @@
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
 
 const SectionWrapperPC = (Component, idName) =>
 	function HOC() {
+		const [key, setKey] = useState(0);
+
+		function handleResize() {
+			setKey((prevKey) => prevKey + 1);
+		}
+
+		useEffect(() => {
+			window.addEventListener("resize", handleResize);
+			return () => {
+				window.removeEventListener("resize", handleResize);
+			};
+		}, []);
+
 		return (
 			<motion.section
+				key={key}
 				variants={staggerContainer()}
 				initial="hidden"
-				whileInView={"show"}
-				viewport={{
-					once: true,
-					amount: 0.01,
-				}}
+				animate="show"
+				// whileInView={"show"}
+				// viewport={{
+				// 	once: true,
+				// 	amount: 0.01,
+				// }}
 				className={`${styles.paddingPC} max-w-7xl mx-auto relative z-0`}
 			>
 				{/* <span className="hash-span" id={idName}>
