949ef70c5816d9590f022c9ee980db0c1f4a8972
yashPundhir <yash.pundhir.prof@gmail.com>
2023-05-01 20:16:40 +0530

    Hero Section's Heading & Sub-Heading styling is updated

--- src/styles.js
+++ src/styles.js
@@ -4,10 +4,14 @@ const styles = {
 	padding: "sm:px-16 px-6 sm:py-16 py-10",
 	paddingPC: "sm:px-16 px-6 sm:pb-16 pb-10 sm:pt-4 pt-2",
 
+	// heroHeadText:
+	// 	"font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
 	heroHeadText:
-		"font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+		"font-black text-white lg:text-[90px] sm:text-[70px] xs:text-[60px] text-[50px] lg:leading-[115px] mt-2",
+	// heroSubText:
+	// 	"text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
 	heroSubText:
-		"text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
+		"text-[#dfd9ff] font-medium lg:text-[40px] sm:text-[32px] xs:text-[24px] text-[18px] lg:leading-[49px]",
 
 	sectionHeadText:
 		"text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
